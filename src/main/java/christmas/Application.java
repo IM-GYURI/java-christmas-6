8efadbd3f0bf5c94d05fa25a5310eb21699d7456
IM-GYURI <kgulr0517@gmail.com>
2024-11-25 16:43:08 +0900

    feat : 메뉴 세팅을 Application에서 호출하도록 한다.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,9 +2,12 @@ package christmas;
 
 import christmas.config.AppConfig;
 import christmas.controller.ChristmasController;
+import christmas.service.MenuInitializer;
 
 public class Application {
     public static void main(String[] args) {
+        MenuInitializer.initMenuSetting();
+
         ChristmasController controller = AppConfig.INSTANCE.getChristmasController();
         controller.start();
     }
