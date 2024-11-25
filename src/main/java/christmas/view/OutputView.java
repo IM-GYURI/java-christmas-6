096a4b425aa8b37dbc261658bfbad4633b2d8237
IM-GYURI <kgulr0517@gmail.com>
2024-11-25 15:20:32 +0900

    feat : 출력을 담당하는 OutputView를 구현한다.
    - printHello() : 환영 문구 출력
    - printError() : 에러 문구 출력

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,12 @@
+package christmas.view;
+
+public class OutputView {
+
+    public static void printHello() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+}
