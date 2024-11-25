1d887c7431009c965a4e9df53b2dd5d335eec5f1
IM-GYURI <kgulr0517@gmail.com>
2024-11-25 16:42:49 +0900

    feat : AppConfig에 Presentation, ChristmasDDay, PriceService, PromotionService 객체 처리를 추가한다.

--- src/main/java/christmas/config/AppConfig.java
+++ src/main/java/christmas/config/AppConfig.java
@@ -1,14 +1,23 @@
 package christmas.config;
 
 import christmas.controller.ChristmasController;
+import christmas.domain.ChristmasDDay;
+import christmas.domain.Presentation;
 import christmas.service.OrderService;
+import christmas.service.PriceService;
+import christmas.service.PromotionService;
 
 public enum AppConfig {
 
     INSTANCE;
 
+    private final Presentation presentation = new Presentation();
+    private final ChristmasDDay christmasDDay = new ChristmasDDay();
     private final OrderService orderService = new OrderService();
-    private final ChristmasController christmasController = new ChristmasController(orderService);
+    private final PriceService priceService = new PriceService();
+    private final PromotionService promotionService = new PromotionService(presentation, christmasDDay);
+    private final ChristmasController christmasController = new ChristmasController(
+            orderService, priceService, promotionService);
 
     public ChristmasController getChristmasController() {
         return christmasController;
