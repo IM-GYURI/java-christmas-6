80c84f15ae48f8679ec782d20c4e0e4299b1307d
IM-GYURI <kgulr0517@gmail.com>
2024-11-25 15:20:54 +0900

    feat : 재입력을 처리하는 RetryHandler를 구현한다.
    - Supplier<T>
    - Runnable

---
+++ src/main/java/christmas/util/RetryHandler.java
@@ -0,0 +1,36 @@
+package christmas.util;
+
+import christmas.exception.ErrorMessage;
+import christmas.view.OutputView;
+import java.util.function.Supplier;
+
+public class RetryHandler {
+    private static final int MAX_ATTEMPTS = 5;
+
+    public static <T> T handleRetry(final Supplier<T> supplier) {
+        int attempts = 0;
+        while (attempts < MAX_ATTEMPTS) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                attempts++;
+                OutputView.printError(e.getMessage());
+            }
+        }
+        throw new IllegalArgumentException(ErrorMessage.TOO_MANY_INVALID_INPUT.getMessage());
+    }
+
+    public static void handleRetry(final Runnable runnable) {
+        int attempts = 0;
+        while (attempts < MAX_ATTEMPTS) {
+            try {
+                runnable.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                attempts++;
+                OutputView.printError(e.getMessage());
+            }
+        }
+        throw new IllegalArgumentException(ErrorMessage.TOO_MANY_INVALID_INPUT.toString());
+    }
+}
