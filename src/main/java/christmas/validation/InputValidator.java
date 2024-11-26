fb1b1ce00a8b6fe65c9561f17e9915600fd10b22
IM-GYURI <kgulr0517@gmail.com>
2024-11-27 04:07:47 +0900

    refactor : 주문 입력에 대한 정규식 검증, 주문 목록에 대한 검증을 InputValidator로 이동시킨다.

--- src/main/java/christmas/validation/InputValidator.java
+++ src/main/java/christmas/validation/InputValidator.java
@@ -1,18 +1,50 @@
 package christmas.validation;
 
+import christmas.domain.Order;
 import christmas.exception.ErrorMessage;
+import christmas.repository.MenuRepository;
+import christmas.repository.OrderRepository;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class InputValidator {
+
+    private static final String ORDER_REGEX = "([가-힣a-zA-Z0-9]+-\\d+)(,([가-힣a-zA-Z0-9]+-\\d+))*";
 
     public static void validateInput(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
         }
     }
+    
+    public static void validateRegex(String input) {
+        if (!Pattern.compile(ORDER_REGEX).matcher(input).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+        }
+    }
 
-    public static void validateDate(int date) {
+    public void validateDate(int date) {
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DATE.getMessage());
         }
+    }
+
+    public void validateOrders(List<Order> orders) {
+        if (orders.stream()
+                .map(Order::getName)
+                .distinct()
+                .count() != orders.size()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+        }
+
+        orders.forEach(order -> {
+            if (OrderRepository.exists(order.getName())) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+            }
+
+            if (!MenuRepository.exists(order.getName())) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+            }
+        });
     }
 }
