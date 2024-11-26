845817bffe71165e0cfdb49fd015419e68d4eef4
IM-GYURI <kgulr0517@gmail.com>
2024-11-27 04:05:55 +0900

    refactor : 컨트롤러의 의존성을 추가하고, PreviewDto를 생성하는 로직을 서비스로 이동시킨다.

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,66 +1,43 @@
 package christmas.controller;
 
 import christmas.domain.Order;
-import christmas.dto.PresentationDto;
 import christmas.dto.PreviewDto;
-import christmas.dto.PromotionDetailDto;
 import christmas.repository.OrderRepository;
-import christmas.service.OrderService;
 import christmas.service.PriceService;
 import christmas.service.PromotionService;
 import christmas.util.RetryHandler;
-import christmas.view.InputView;
 import christmas.view.OutputView;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class ChristmasController {
 
-    private final OrderService orderService;
+    private final RetryHandler retryHandler;
     private final PriceService priceService;
     private final PromotionService promotionService;
 
-    public ChristmasController(OrderService orderService, PriceService priceService,
+    public ChristmasController(RetryHandler retryHandler, PriceService priceService,
                                PromotionService promotionService) {
-        this.orderService = orderService;
+        this.retryHandler = retryHandler;
         this.priceService = priceService;
         this.promotionService = promotionService;
     }
 
     public void start() {
         OutputView.printHello();
-        int date = RetryHandler.handleRetry(InputView::askDate);
+        int date = retryHandler.getDate();
         getOrders();
-
         printPreview(date);
     }
 
     private void getOrders() {
-        RetryHandler.handleRetry(() -> {
-            String orders = InputView.askOrder();
-            orderService.createOrders(orders);
-        });
-    }
+        List<Order> orders = retryHandler.getOrders();
+        orders.forEach(OrderRepository::addOrder);
 
-    private List<PromotionDetailDto> getPromotionDetails(int date) {
-        List<PromotionDetailDto> promotionDetails = new ArrayList<>();
-
-        if (promotionService.getChristmasDDayDiscount(date).isPresent()) {
-            promotionDetails.add(promotionService.getChristmasDDayDiscount(date).get());
-        }
-
-        return promotionDetails;
+        OrderRepository.validateOrders();
     }
 
     private void printPreview(int date) {
-        List<Order> orders = OrderRepository.orders();
-        int totalPrice = priceService.getTotalPrice();
-        Optional<PresentationDto> presentation = promotionService.determinePresentation(totalPrice);
-
-        List<PromotionDetailDto> promotionDetails = getPromotionDetails(date);
-
-        PreviewDto preview = new PreviewDto(date, orders, totalPrice, presentation, promotionDetails);
+        PreviewDto preview = promotionService.getPreview(date, priceService.getTotalPrice());
         OutputView.printPreview(preview);
     }
 }
