3f61078e1c149a30acc07fe23deb749d28504545
IM-GYURI <kgulr0517@gmail.com>
2024-11-27 04:15:20 +0900

    feat : RetryHandler에서 날짜 처리와 주문 처리를 담당하도록 한다.
    - 의존성 추가
    - getDate() : 날짜 처리 후 반환
    - getOrders() : 주문 처리 후 반환
    - retryLogics() : 입력 값을 파싱하고 검증하여 반환

--- src/main/java/christmas/util/RetryHandler.java
+++ src/main/java/christmas/util/RetryHandler.java
@@ -1,36 +1,43 @@
 package christmas.util;
 
-import christmas.exception.ErrorMessage;
+import christmas.domain.Order;
+import christmas.validation.InputValidator;
+import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class RetryHandler {
-    private static final int MAX_ATTEMPTS = 5;
 
-    public static <T> T handleRetry(final Supplier<T> supplier) {
-        int attempts = 0;
-        while (attempts < MAX_ATTEMPTS) {
-            try {
-                return supplier.get();
-            } catch (IllegalArgumentException e) {
-                attempts++;
-                OutputView.printError(e.getMessage());
-            }
-        }
-        throw new IllegalArgumentException(ErrorMessage.TOO_MANY_INVALID_INPUT.getMessage());
+    private final InputParser inputParser;
+    private final InputValidator inputValidator;
+
+    public RetryHandler(InputParser inputParser, InputValidator inputValidator) {
+        this.inputParser = inputParser;
+        this.inputValidator = inputValidator;
     }
 
-    public static void handleRetry(final Runnable runnable) {
-        int attempts = 0;
-        while (attempts < MAX_ATTEMPTS) {
+    public int getDate() {
+        return retryLogics(InputView::askDate, inputParser::parseDate, inputValidator::validateDate);
+    }
+
+    public List<Order> getOrders() {
+        return retryLogics(InputView::askOrder, inputParser::parseOrders, inputValidator::validateOrders);
+    }
+
+    public <T> T retryLogics(Supplier<String> input, Function<String, T> parser,
+                             Consumer<T> validator) {
+        while (true) {
             try {
-                runnable.run();
-                return;
+                String userInput = input.get();
+                T parsedInput = parser.apply(userInput);
+                validator.accept(parsedInput);
+                return parsedInput;
             } catch (IllegalArgumentException e) {
-                attempts++;
                 OutputView.printError(e.getMessage());
             }
         }
-        throw new IllegalArgumentException(ErrorMessage.TOO_MANY_INVALID_INPUT.toString());
     }
 }
