ad67fe6e0c5700e1446f93985196414facf0bf6c
IM-GYURI <kgulr0517@gmail.com>
2024-11-27 04:14:05 +0900

    feat : PromotionType에 주말 할인을 추가한다.

--- src/main/java/christmas/type/PromotionType.java
+++ src/main/java/christmas/type/PromotionType.java
@@ -3,7 +3,8 @@ package christmas.type;
 public enum PromotionType {
     CHRISTMAS_D_DAY("크리스마스 디데이 할인"),
     WEEKDAY("평일 할인"),
-    WEEKEND("특별 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인"),
     PRESENT("증정 이벤트");
 
     final String type;
